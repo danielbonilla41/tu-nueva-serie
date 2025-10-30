fa534ead7650e2f923a8b1ee643f087c6d76f373
Daniel Bonilla <danielbonilla41@gmail.com>
2025-10-30 17:18:12 -0500

    Se agrega boton de whatsapp

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,12 @@ import { Component, inject, computed, Signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { DataService } from './core/services/data.service';
+import { WhatsappFloat } from './shared/whatsapp-float/whatsapp-float';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, CommonModule],
+  imports: [RouterOutlet, RouterLink, CommonModule, WhatsappFloat],
   templateUrl: './app.html',
   styleUrls: ['./app.css'],
 })
