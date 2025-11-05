5ad34caa278853ff7c46515637b33b41b199695b
Daniel Bonilla <danielbonilla41@gmail.com>
2025-11-04 22:07:29 -0500

    Se ajusta modal de la pagina de cuentas

--- src/app/pages/accounts/feature-modal/feature-modal.ts
+++ src/app/pages/accounts/feature-modal/feature-modal.ts
@@ -12,23 +12,27 @@ import { PriceAccount } from '../../../models/price-account.model';
 export class FeatureModal implements OnChanges {
   // Datos de la plataforma a mostrar
   @Input() platform: PriceAccount | null = null;
-
   // Evento para notificar al padre que el modal debe cerrarse
   @Output() close = new EventEmitter<void>();
-
   // Referencia al elemento <dialog> nativo en la plantilla
   @ViewChild('modalDialog') dialog!: ElementRef<HTMLDialogElement>;
 
   // Detecta cuando el Input 'platform' cambia
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['platform']) {
-      if (this.platform) {
-        // Si el Input es una plataforma, abrimos el modal
-        this.dialog.nativeElement.showModal();
-      } else if (this.dialog?.nativeElement.open) {
-        // Si el Input es null (se cerró), cerramos el modal
-        this.dialog.nativeElement.close();
-      }
+      this.setModalState();
+    }
+  }
+
+  // MÉTODO PARA GESTIONAR EL ESTADO
+  private setModalState(): void {
+    if (!this.dialog?.nativeElement) {
+      return;
+    }
+    if (this.platform) {
+      this.dialog.nativeElement.showModal();
+    } else if (this.dialog.nativeElement.open) {
+      this.dialog.nativeElement.close();
     }
   }
 
